7466c78d8661172f5b2e4735daf3cfb0864de030
Andrei_Novikov <novikovas1@mail.ru>
2023-01-26 22:18:54 +0300

    Занятие №3_Лабораторные задания

---
+++ Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
@@ -0,0 +1,89 @@
+from typing import Union
+
+
+class Book:
+    """ Базовый класс книги. """
+
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        if not isinstance(name, str):
+            raise TypeError("Название может быть только типа str")
+        self.__name = name
+
+    @property
+    def author(self) -> str:
+        return self.__author
+
+    @author.setter
+    def author(self, author: str):
+        if not isinstance(author, str):
+            raise TypeError("Автор может быть только типа str")
+        self.__author = author
+
+    def __str__(self) -> str:
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}, кол-во страниц: {self.pages}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Кол-во страниц может быть только целым положительным числом")
+        if value < 1:
+            raise ValueError("Кол-во страниц может быть только целым положительным числом")
+        self._pages = value
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}, Продолжительность: {self.duration}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, value: Union[int, float]):
+        if not isinstance(value, (int, float)):
+            raise TypeError("Продолжительность может быть только положительным числом")
+        if value <= 0:
+            raise ValueError("Продолжительность может быть только положительным числом")
+        self._duration = float(value)
+
+
+if __name__ == "__main__":
+    a = PaperBook("LOtR", "Tolkien", 22)
+    b = AudioBook('HarryPotter', 'Joanne Rowling', 1)
+    c = Book("HP", 'Joanne Rowling')
